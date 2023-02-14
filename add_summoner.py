c6e9abac0f8653cc396ac0de89af6250d4551b60
Renja <renja.grotemeyer@gmail.com>
2023-02-14 02:32:50 +0100

    Added rank to match

--- add_summoner.py
+++ add_summoner.py
@@ -3,11 +3,6 @@ import json
 import os
 from pymongo import MongoClient
 
-'''
-When The Summoner is getting added the last Match also has to be added
-so that the program can check if a new match has been played.
-'''
-
 api_key = os.getenv('RIOT_API_KEY')
 watcher = LolWatcher(api_key)
 mongodb_pw = os.getenv('MONGODB_PW')
@@ -15,19 +10,48 @@ mongodb_pw = os.getenv('MONGODB_PW')
 CONNECTION_STRING = f'mongodb+srv://user:{mongodb_pw}@rank-crawler.kvodu7l.mongodb.net/?retryWrites=true&w=majority'
 client = MongoClient(CONNECTION_STRING)
 
-summoner_name_input = input('Enter summoner name: ')
-summoner_region_input = input('Enter region: ')
-# BR1, EUN1, EUW1, JP1, KR, LA1, LA2, NA1, OC1, TR1, RU, PH2, SG2, TH2, TW2, VN2
+def get_last_match_id(summoner_region, summoner_puuid):
+    my_matches = watcher.match.matchlist_by_puuid(summoner_region, summoner_puuid)
+    return my_matches[0]
 
+def add_summoner_to_db(summoner_data):
+    rank_crawler_db = client['rank-crawler']
+    rank_crawler_db.summoner.insert_one(summoner_data)
 
+def get_summoner_rank(summoner_region, summoner_id):
+    summoner_rank = watcher.league.by_summoner(summoner_region, summoner_id)
+    return summoner_rank
+
+def add_last_match_to_db(last_match):
+    rank_crawler_db = client['rank-crawler']
+    # If the match is a Ranked game 420 or 440
+    # for every participant in the match
+    # check if the summoner is saved in the database
+    # if yes get the current rank for the summoner and save it to the participant object
+    # queueType = RANKED_FLEX_SR == [0]
+    # queueType = RANKED_SOLO_5x5 == [1]
+    if last_match['info']['queueId'] == 420:
+        for participant in last_match['info']['participants']:
+            summoner = rank_crawler_db.summoner.find_one({'puuid': participant['puuid']})
+            if summoner:
+                league = get_summoner_rank(summoner_region_input, summoner['id'])[1]
+                tier = league['tier']
+                rank = league['rank']
+                leaguePoints = league['leaguePoints']
+
+                participant['rank'] = {
+                    'tier': tier,
+                    'rank': rank,
+                    'leaguePoints': leaguePoints
+                }
+    rank_crawler_db.match.insert_one(last_match)
 
 
 def add_summoner(summoner_name, summoner_region):
     summoner = watcher.summoner.by_name(summoner_region, summoner_name)
-    my_matches = watcher.match.matchlist_by_puuid(summoner_region, summoner['puuid'])
-    last_match_id = my_matches[0]
-    rank_crawler_db = client['rank-crawler']
+    last_match_id = get_last_match_id(summoner_region, summoner['puuid'])
 
+    rank_crawler_db = client['rank-crawler']
     existing_summoner = rank_crawler_db.summoner.find_one({'puuid': summoner['puuid']})
 
     if existing_summoner:
@@ -38,10 +62,11 @@ def add_summoner(summoner_name, summoner_region):
     summoner_data['matches'] = []
     summoner_data['matches'].insert(0, last_match_id)
 
-    rank_crawler_db.summoner.insert_one(summoner_data)
-    
+    add_summoner_to_db(summoner_data)
+
     last_match = watcher.match.by_id(summoner_region, last_match_id)
-    rank_crawler_db.match.insert_one(last_match)
+    add_last_match_to_db(last_match)
 
-
+summoner_name_input = input('Enter summoner name: ')
+summoner_region_input = input('Enter region: ')
 add_summoner(summoner_name_input, summoner_region_input)
