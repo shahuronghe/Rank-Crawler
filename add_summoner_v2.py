2f613d9fe652194b97208f286bfa2ea6a02cc533
Renja <renja.grotemeyer@gmail.com>
2023-08-09 17:45:27 +0200

    Finished add_summoner function

--- add_summoner_v2.py
+++ add_summoner_v2.py
@@ -31,19 +31,38 @@ cass.set_riot_api_key(RIOT_API_KEY)
 
 
 def save_summoner(summoner_name: str, platform: str):
+    '''Saves a summoner to the database.'''
     # Ckeck if summoner is already in database
-    '''
     collection = client['rank-crawler']['summoners']
     summoner = collection.find_one({'name': summoner_name, 'platform': platform})
     if summoner:
         print('Summoner already in database!')
         return
-    '''
 
-    # Get summoner data from Riot API
+
+    # Get summoner data
     cass_summoner = cass.get_summoner(name=summoner_name, region=platform_to_cass_region(platform))
-
     cass_summoner.load()
-    print(cass_summoner)
+    summoner_dict = cass_summoner.to_dict()
+    summoner_dict.pop('region')
+    summoner_dict['platform'] = platform
+
+
+    # Get the rank data of the summoner
+    cass_rank = cass_summoner.league_entries
+    rank_dict = []
+    for rank in cass_rank:
+        rank_dict.append(rank.to_dict())
+
+        # Remove unnecessary data from rank_dict
+        rank_dict[-1].pop('summonerName')
+        rank_dict[-1].pop('summonerId')
+        rank_dict[-1].pop('region')
+    summoner_dict['rank'] = rank_dict
+
+    # Save summoner to database
+    summoner_collection = client['rank-crawler']['summoners']
+    summoner_collection.insert_one(summoner_dict)
+
 
 save_summoner('G5 Easy', 'euw1')
